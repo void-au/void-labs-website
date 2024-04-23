b35f04ca8c8a9e2419ebc04748bce8a2d4f06ba2
mattboan <matt@voidstudios.com.au>
2024-04-23 14:50:37 +0800

    Added meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./styles/index.scss"
 import { Header } from "@/comps/Header";
 import { Footer } from "@/comps/Footer";
 import GoogleAnalytics from "@/comps/GoogleAnalytics";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Void Labs | Australia",
@@ -15,9 +16,26 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <GoogleAnalytics />
+      <Script
+        id='meta-pixel'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '730865832281471');
+        fbq('track', 'PageView');`
+        }}
+      />
       <Header />
       <body>{children}</body>
       <Footer />
