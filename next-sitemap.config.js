e0d78c7839606ab59e96d707971615e0c29069e5
mattboan <matt@voidstudios.com.au>
2024-05-11 08:13:08 +0800

    Added sitemap functioanlity

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://voidlabs.com.au',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
